73b05fd8467467944450a3512fd370cb99206c81
Salvatore Diana <diasalvatore@gmail.com>
2016-07-08 09:16:39 +0200

    Formatted command line

--- src/main/java/com/thore/L2SMT/LCommandLineParser.java
+++ src/main/java/com/thore/L2SMT/LCommandLineParser.java
@@ -9,17 +9,17 @@ public class LCommandLineParser {
     private CommandLine cmd = null;
 
     public LCommandLineParser() {
-        options.addOption("d", "debug", false, "Show debug informations");
-        options.addOption("dd", "extradebug", false, "Show translations");
-        options.addOption("h", "help", false, "Prints this help");
-        options.addOption("o", "output", true, "Writes output in specified files (default is stdout)");
-        options.addOption("q", "quiet", false, "Don't show output (it should be used with -z3 or -x)");
+        options.addOption("d", "debug",        false, "Show debug informations");
+        options.addOption("dd", "extradebug",  false, "Show translations");
+        options.addOption("h", "help",         false, "Prints this help");
+        options.addOption("o", "output",        true, "Writes output in specified files (default is stdout)");
+        options.addOption("q", "quiet",        false, "Don't show output (it should be used with -z3 or -x)");
         options.addOption("i", "ignore-unsat", false, "Ignore unsat and don't stop when a formula is unsat (useful with -z3 and incremental verification) ");
-        options.addOption("x", "execute", true, "Execute command on SMT output");
-        options.addOption("z3", null, false, "Runs z3 on SMT output");
-        options.addOption("f", "format", true, "Specifies input format (xml or l), default is auto guessing base on file extension (.xml or .l)");
-        options.addOption(null, "xml", false, "Force parsing as XML file");
-        options.addOption(null, "l", false, "Force parsing as L file");
+        options.addOption("x", "execute",       true, "Execute command on SMT output");
+        options.addOption("z3", null,          false, "Runs z3 on SMT output");
+        options.addOption("f", "format",        true, "Specifies input format (xml or l), default is auto guessing base on file extension (.xml or .l)");
+        options.addOption(null, "xml",         false, "Force parsing as XML file");
+        options.addOption(null, "l",           false, "Force parsing as L file");
     }
 
     public CommandLine parse(String args[]) {
